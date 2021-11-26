8487da969d009bc51aa0ffa3744fa5c82fe49ccd
japeto <jeffersonamado@gmail.com>
2021-11-25 19:22:29 -0500

    docs(swagger): Add swagger initial settings

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 
-const userRoutes = require("./routes/users");
+const swaggerJsDoc =  require("swagger-jsdoc");
+const swaggerUI =  require("swagger-ui-express");
+const swaggerDocument =  require("../swagger.json");
+
+const specs =  swaggerJsDoc(swaggerDocument);
+
+const userRoutes = require("./users");
 app.use("/users", userRoutes);
 
 app.get("/", (req, resp)=>{
